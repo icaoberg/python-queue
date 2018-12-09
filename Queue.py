94a2a6a0620b0705f0eb41fb360b0085b2102960
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2018-12-09 15:31:16 -0500

    Basic, undocumented class

---
+++ Queue.py
@@ -0,0 +1,18 @@
+class Queue:
+    def __init__(self):
+        self.elements = []
+
+    def push(self, element):
+        raise NotImplementedError("Not implemented yet")
+
+    def pop(self):
+        raise NotImplementedError("Not implemented yet")
+
+    def peek(self):
+        raise NotImplementedError("Not implemented yet")
+
+    def isEmpty(self):
+        raise NotImplementedError("Not implemented yet")
+
+    def size(self):
+        raise NotImplementedError("Not implemented yet")
